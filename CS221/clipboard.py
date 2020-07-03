8334c8ab0d3be1f3c6b677cb8cc33593bd607388
LaytLP <layt.mail@gmail.com>
2020-07-03 15:01:47 +0200

    L

---
+++ CS221/clipboard.py
@@ -0,0 +1,15 @@
+
+points = [(2,4),(4,2)]
+def F(w):
+   return sum((x * x - y)**2 for x, y in points)
+def dF(w):
+   return sum(2*(x*y-y)*x for x, y in points)
+
+# Gradient descent
+w = 0
+eta = 0.01
+for t in range(1000):
+   value = F(w)
+   gradient = dF(w)
+   w = w -eta * gradient
+   print("iteration {}: w = {}, F(w) = {}".format(t,w,value))
